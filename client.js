9adaf5cc5130fc2414c3f5555e1bc77163b9f59f
Mithun B <mithun100@gmail.com>
2023-04-12 12:12:59 -0500

    Started a Express server on 3000 port which in turn will call the GRPC client which calls the GRPC server

--- client.js
+++ client.js
@@ -1,3 +1,33 @@
+
+require("appdynamics").profile({
+  controllerHostName: 'xxxx.saas.appdynamics.com',
+  controllerPort: 443,
+  accountName: 'xxxx',
+  accountAccessKey: 'key',
+  controllerSslEnabled: true,
+  applicationName: 'GRPC-Client-Mithun-App',
+  tierName: 'UGRPC-Client-Mithun-tier',
+  nodeName: 'GRPC-Client-Mithun-node',
+  logging: {
+      'logfiles': [{
+              'root_directory': '/tmp/appd/GRPC-Client-Mithun',
+              'filename': 'echo_%N.log',
+              'level': 'TRACE',
+              'max_size': 5242880,
+              'max_files': 10
+          },
+          {
+              'root_directory': '/tmp/appd/GRPC-Client-Mithun',
+              'filename': 'protobuf_%N.log',
+              'level': 'TRACE',
+              'max_size': 5242880,
+              'max_files': 10,
+              'channel': 'protobuf'
+          }
+      ]
+  }
+})
+//====
 // client.js
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
@@ -5,6 +35,15 @@ const protoLoader = require('@grpc/proto-loader');
 // Load the protobuf definition
 const packageDefinition = protoLoader.loadSync('./example.proto');
 const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+
+
+// Create a continous server which will call the client and it will run on 3000
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+
 
 // Create a gRPC client
 const client = new protoDescriptor.example.ExampleService('localhost:50051', grpc.credentials.createInsecure());
@@ -17,4 +56,17 @@ client.greet({ name: 'John' }, (error, response) => {
     console.log(response.message);
   }
 });
+
+
+
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
 
--- server.js
+++ server.js
@@ -1,3 +1,32 @@
+require("appdynamics").profile({
+  controllerHostName: 'xxxx.saas.appdynamics.com',
+  controllerPort: 443,
+  accountName: 'xxxx',
+  accountAccessKey: 'key',
+  controllerSslEnabled: true,
+  applicationName: 'GRPC-Server-Mithun-App',
+  tierName: 'UGRPC-Server-Mithun-tier',
+  nodeName: 'GRPC-Server-Mithun-node',
+  logging: {
+      'logfiles': [{
+              'root_directory': '/tmp/appd/GRPC-Server-Mithun',
+              'filename': 'echo_%N.log',
+              'level': 'TRACE',
+              'max_size': 5242880,
+              'max_files': 10
+          },
+          {
+              'root_directory': '/tmp/appd/GRPC-Server-Mithun',
+              'filename': 'protobuf_%N.log',
+              'level': 'TRACE',
+              'max_size': 5242880,
+              'max_files': 10,
+              'channel': 'protobuf'
+          }
+      ]
+  }
+})
+
 // server.js
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
